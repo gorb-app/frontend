67f98735ee7e11355486a4822a3223196e4fa968
SauceyRed <saucey@saucey.red>
2025-05-01 22:34:22 +0200

    refactor: remove default.vue layout and move contents back to app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
 <template>
-  <NuxtLayout />
+  <NuxtPage />
 </template>
+
+<style>
+html,
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  box-sizing: border-box;
+  color: rgb(190, 190, 190);
+  background-color: rgb(30, 30, 30);
+  height: 100%;
+  margin: 0;
+}
+
+a {
+  color: aquamarine;
+}
+</style>
