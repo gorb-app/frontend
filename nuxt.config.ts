8b719a9a63b5e7e58e6ed68a52ce3399472dfadc
SauceyRed <saucey@saucey.red>
2025-05-01 22:31:58 +0200

    chore: update dependencies and Nuxt site config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', '@nuxt/image', "@pinia/nuxt"],
+  app: {
+    /*
+      Defines what prefix the client runs on
+      E.g.: baseURL set to "/web" would host at https://gorb.app/web
+      Default is "/" (aka root), which hosts at https://gorb.app/
+    */
+    baseURL: "/",
+  },
+  runtimeConfig: {
+    public: {
+      apiVersion: 1
+    }
+  }
 })
