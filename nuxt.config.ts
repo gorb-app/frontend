986c5a90eba09d420529235b26f4dc0c62189cef
SauceyRed <saucey@saucey.red>
2025-05-26 22:36:29 +0200

    feat: add Nuxt Icon module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,28 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', "@pinia/nuxt"],
+  modules: ['@nuxt/eslint', '@nuxt/image', "@pinia/nuxt", "@nuxt/icon"],
   app: {
     /*
       Defines what prefix the client runs on
       E.g.: baseURL set to "/web" would host at https://gorb.app/web
       Default is "/" (aka root), which hosts at https://gorb.app/
     */
-    baseURL: "/",
+    baseURL: "/"
   },
   runtimeConfig: {
     public: {
       apiVersion: 1
     }
-  }
+  },
+  /* nitro: {
+    devProxy: {
+      "/api": {
+        target: "http://localhost:8080/api",
+        changeOrigin: true,
+        prependPath: true,
+        ws: true
+      }
+    }
+  } */
 })
