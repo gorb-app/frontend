c9decc585ef75559dad45cd02fe5245a2ffd90c4
SauceyRed <saucey@saucey.red>
2025-05-26 22:46:13 +0200

    feat: add server page

--- pages/servers/[serverId]/index.vue
+++ pages/servers/[serverId]/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    
+    Hello
   </div>
 </template>
 
 <script lang="ts" setup>
+const server = await fetchWithApi(`/servers/${useRoute().params.serverId}`);
+console.log("server:", server);
 
 </script>
 
