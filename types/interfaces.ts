89cd8ec1bfe6a9626d1be65b629a65ee88998cfc
SauceyRed <saucey@saucey.red>
2025-05-26 22:40:56 +0200

    feat: add file for storing types

---
+++ types/interfaces.ts
@@ -0,0 +1,54 @@
+export interface ChannelPermissionResponse {
+	channel_uuid: string,
+	role_uuid: string,
+	permissions: number
+}
+
+export interface RoleResponse {
+	uuid: string,
+	guild_uuid: string,
+	name: string,
+	color: number,
+	position: number,
+	permissions: number
+}
+
+export interface GuildResponse {
+    uuid: string,
+    name: string,
+    description: string | null,
+    icon: string | null,
+    owner_uuid: string,
+    roles: [],
+    member_count: number
+}
+
+export interface ChannelResponse {
+	uuid: string,
+	guild_uuid: string,
+	name: string,
+	description: string,
+	permissions: ChannelPermissionResponse[]
+}
+
+export interface MessageResponse {
+	uuid: string
+	channel_uuid: string
+	user_uuid: string
+	message: string
+}
+
+export interface InviteResponse {
+	id: string,
+	user_uuid: string,
+	guild_uuid: string
+}
+
+export interface UserResponse {
+	uuid: string,
+	username: string,
+	display_name: string | null,
+	avatar: string | null,
+	email: string,
+	email_verified: boolean
+  }
