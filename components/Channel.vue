1d79f680dfbaf6bad795947fbd843d8ca943c00f
SauceyRed <saucey@saucey.red>
2025-05-26 22:48:11 +0200

    feat: add Channel component

--- components/Channel.vue
+++ components/Channel.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-	<a class="channel-list-link" :href="href">
-		{{ name }}
-	</a>
+  <div v-if="current" class="channel-list-link-container">
+	<NuxtLink class="channel-list-link" :href="props.href">
+		# {{ name }}
+	</NuxtLink>
+  </div>
+  <div v-else class="channel-list-link-container current-channel">
+	<NuxtLink class="channel-list-link" :href="props.href">
+		# {{ name }}
+	</NuxtLink>
   </div>
 </template>
 
 <script lang="ts" setup>
 
-const props = defineProps<{ name: string, href: string }>();
+const props = defineProps<{ name: string, href: string, current?: boolean }>();
 
 </script>
 
@@ -16,5 +21,18 @@ const props = defineProps<{ name: string, href: string }>();
 .channel-list-link {
 	text-decoration: none;
 	color: inherit;
+	padding-left: .5dvw;
+	padding-right: .5dvw;
+}
+
+.channel-list-link-container {
+	text-align: left;
+	display: flex;
+	height: 4dvh;
+	white-space: nowrap;
+}
+
+.current-channel {
+	background-color: rgb(70, 70, 70);
 }
 </style>
