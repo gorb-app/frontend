68cd8e10edbcac5f0eb7a132fcd7402836b2291a
SauceyRed <saucey@saucey.red>
2025-05-26 22:47:11 +0200

    feat: add Message class, may remove later

---
+++ classes/Message.ts
@@ -0,0 +1,19 @@
+import type { MessageResponse } from "~/types/interfaces";
+
+export default class Message {
+	uuid: string;
+	channelUuid: string;
+	userUuid: string;
+	message: string;
+
+	constructor({ uuid, channel_uuid, user_uuid, message }: MessageResponse) {
+		this.uuid = uuid;
+		this.channelUuid = channel_uuid;
+		this.userUuid = user_uuid;
+		this.message = message;
+	}
+
+	getTimestamp() {
+		return uuidToTimestamp(this.uuid);
+	}
+}
