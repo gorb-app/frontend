22ca450651c9ac82e594e487f1a8b6960a927535
SauceyRed <saucey@saucey.red>
2025-05-28 02:17:54 +0200

    feat: make Loading component a spinning load circle

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    Loading...
+  <div id="loading-container">
+    <Icon name="lucide:loader-circle" id="loading-circle" />
   </div>
 </template>
 
@@ -8,6 +8,31 @@
 
 </script>
 
-<style>
+<style scoped>
+
+#loading-container {
+	position: fixed;
+	left: 50dvw;
+	top: 50dvh;
+	transform: translate(-50%, -50%);
+}
+
+@keyframes spin {
+	0% {
+		transform: rotate(0deg);
+	}
+
+	100% {
+		transform: rotate(360deg);
+	}
+}
+
+#loading-circle {
+	animation-name: spin;
+	animation-duration: 1s;
+	animation-timing-function: linear;
+	animation-iteration-count: infinite;
+	font-size: 2rem;
+}
 
 </style>
