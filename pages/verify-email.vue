9f6490d744e8879ee6fd79d74d1c5f990967295e
SauceyRed <saucey@saucey.red>
2025-05-28 02:26:17 +0200

    feat: add verify-email page, wip

---
+++ pages/verify-email.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="container">
+		<div v-if="errorMessage">
+			{{ errorMessage }}
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+
+const errorMessage = ref();
+
+const token = useRoute().query.token;
+
+try {
+	const res = await fetchWithApi("/auth/verify-email", { query: { token } });
+	console.log("hi");
+} catch (error) {
+	console.error("Error verifying email:", error);
+	errorMessage.value = error;
+}
+
+</script>
+
+<style scoped>
+
+#container {
+	text-align: center;
+	margin: auto;
+}
+
+</style>
