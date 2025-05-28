a15f85a08276b3824890a1c7fbd7ede015180761
SauceyRed <saucey@saucey.red>
2025-05-28 02:24:54 +0200

    fix: refresh returning 401 not properly logging you out of client

--- utils/fetchWithApi.ts
+++ utils/fetchWithApi.ts
@@ -9,63 +9,74 @@ export default async <T>(path: string, options: NitroFetchOptions<string> = {}) 
     path = path.slice(0, path.lastIndexOf("/"));
   }
   console.log("formatted path:", path);
-  try {
-    const accessToken = useCookie("access_token");
-    console.log("access token:", accessToken.value);
-    const apiBase = useCookie("api_base").value;
-    const apiVersion = useRuntimeConfig().public.apiVersion;
-    console.log("heyoooo")
-    console.log("apiBase:", apiBase);
-    if (!apiBase) {
-      console.log("no api base");
-      return;
-    }
-    console.log("path:", path)
-    const { revoke, refresh } = useAuth();
-    console.log("access token 2:", accessToken.value);
-  
-    let headers: HeadersInit = {};
-  
-    if (accessToken.value) {
-      headers = {
-        ...options.headers,
-        "Authorization": `Bearer ${accessToken.value}`
-      };
-    } else {
-      headers = {
-        ...options.headers
-      };
-    }
-  
-    let reauthFailed = false;
-    while (!reauthFailed) {
-      try {
-        console.log("fetching:", URL.parse(apiBase + path));
-        const res = await $fetch<T>(URL.parse(apiBase + path)!.href, {
-          ...options,
-          headers,
-          credentials: "include"
-        });
-    
-        return res;
-      } catch (error: any) {
-        if (error?.response?.status === 401) {
-          if (!path.startsWith("/auth/refresh")) {
-            try {
-              await refresh();
-            } catch (error: any) {
-              if (error?.response?.status === 401) {
-                reauthFailed = true;
-                await revoke();
-                return;
-              }
-            }
-          }
-        }
-        throw error;
-      }
-    }
-  } catch (error) {
-    console.error("error:", error);
-  }
+	const accessToken = useCookie("access_token");
+	console.log("access token:", accessToken.value);
+	const apiBase = useCookie("api_base").value;
+	const apiVersion = useRuntimeConfig().public.apiVersion;
+	console.log("heyoooo")
+	console.log("apiBase:", apiBase);
+	if (!apiBase) {
+		console.log("no api base");
+		return;
+	}
+	console.log("path:", path)
+	const { revoke, refresh } = useAuth();
+	console.log("access token 2:", accessToken.value);
+
+	let headers: HeadersInit = {};
+
+	if (accessToken.value) {
+		headers = {
+		...options.headers,
+		"Authorization": `Bearer ${accessToken.value}`
+		};
+	} else {
+		headers = {
+		...options.headers
+		};
+	}
+
+	let reauthFailed = false;
+	while (!reauthFailed) {
+		try {
+		console.log("fetching:", URL.parse(apiBase + path));
+		const res = await $fetch<T>(URL.parse(apiBase + path)!.href, {
+			...options,
+			headers,
+			credentials: "include"
+		});
+
+		return res;
+		} catch (error: any) {
+		console.error("Error fetching resource");
+		if (error?.response?.status === 401) {
+			console.log("Error status is 401");
+			if (!path.startsWith("/auth/refresh")) {
+			console.log("Path is not refresh endpoint");
+			try {
+				console.log("Trying to refresh");
+				await refresh();
+				console.log("Successfully refreshed token");
+			} catch (error: any) {
+				console.log("Failed to refresh token");
+				if (error?.response?.status === 401) {
+				console.log("Refresh returned 401");
+				reauthFailed = true;
+				console.log("Revoking");
+				await revoke();
+				console.log("Redirecting to login");
+				await navigateTo("/login");
+				console.log("redirected");
+				return;
+				}
+			}
+			} else {
+			console.log("Path is refresh endpoint, throwing error");
+			throw error;
+			}
+		}
+		console.log("throwing error");
+		throw error;
+		}
+	}
 }
