705b37fa06ab647a447295e4175f00bc1309bf66
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-06-01 06:13:00 +0200

    feat: button components

---
+++ components/buttons/Button.vue
@@ -0,0 +1,43 @@
+<!--if you want a button with a different colour, you have to inline CSS or do something similar
+
+    <Button text="Reset Password" :callback=reset_password style="color: purple; background-color: blue"></Button>
+
+	if you have multiple buttons with the same style, feel free to make another component
+
+    this is because i couldn't find a way of dynamically taking in colours without using javascript
+	to update the colour during runtime, i REFUSE to do this due to performance -->
+
+<template>
+	<div id="button" @click="props.callback()">
+		{{ props.text }}
+	</div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+	text: string,
+	callback: CallableFunction,
+}>();
+
+</script>
+
+<style scoped>
+#button {
+	cursor: pointer;
+
+	background-color: #b35719;
+	color: #ffffff;
+
+	padding: 8px 16px;
+	font-size: 16px;
+	transition: background-color 0.2s;
+
+	border-radius: 16px;
+	text-decoration: none;
+	display: inline-block;
+}
+
+#button:hover {
+	background-color: #934410;
+}
+</style>
