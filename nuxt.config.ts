c1021b11922a423bb233fef5e18ed38a8e5d100f
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-07-05 15:24:09 +0200

    fix: fix build time and version hash being evaluated at runtime

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,9 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiVersion: 1,
-      messageGroupingMaxDifference: 300000
+      messageGroupingMaxDifference: 300000,
+      buildTimeString: new Date().toISOString(),
+      gitHash: process.env.GIT_SHORT_REV || "N/A",
     }
   },
   /* nitro: {
