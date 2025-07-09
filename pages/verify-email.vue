84aa1f95fe555a60ff0f23cb8e4e320afaf3e090
SauceyRed <saucey@saucey.red>
2025-07-09 07:30:06 +0200

    feat: add auto redirection in verify-email page

--- pages/verify-email.vue
+++ pages/verify-email.vue
@@ -15,6 +15,12 @@ const token = useRoute().query.token;
 try {
 	const res = await fetchWithApi("/auth/verify-email", { query: { token } });
 	console.log("hi");
+	const query = useRoute().query;
+	if (query.redirect_to) {
+		await navigateTo(`/?redirect_to=${query.redirect_to}`);
+	} else {
+		await navigateTo("/");
+	}
 } catch (error) {
 	console.error("Error verifying email:", error);
 	errorMessage.value = error;
