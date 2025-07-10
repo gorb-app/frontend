8a9ecaa2e2ead30576367a2bd275c70edeb1da19
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-07-10 22:44:18 +0200

    refactor: move spacer into it's own component

---
+++ components/VerticalSpacer.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="spacer"></span>
+</template>
+
+<script lang="ts" setup>
+import type { UserResponse } from '~/types/interfaces';
+
+const props = defineProps<{
+	user: UserResponse,
+}>();
+
+</script>
+
+<style scoped>
+.spacer {
+  height: 0.2dvh;
+  display: block;
+  margin: 0.8dvh 0.2dvw;
+  background-color: var(--padding-color);
+}
+</style>
