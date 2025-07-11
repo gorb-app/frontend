2ff892b0da73a9d0e1ad5522e2dece2f74c7a16f
SauceyRed <saucey@saucey.red>
2025-07-11 03:31:25 +0200

    feat: add util to create MessageReply instance

--- utils/replyToMessage.ts
+++ utils/replyToMessage.ts
@@ -1,5 +1,14 @@
+import { render } from "vue";
+import MessageReply from "~/components/MessageReply.vue";
 import type { MessageProps } from "~/types/props";
 
 export default (element: HTMLDivElement, props: MessageProps) => {
 	console.log("element:", element);
+	const messageBox = document.getElementById("message-box") as HTMLDivElement;
+	if (messageBox) {
+		const div = document.createElement("div");
+		const messageReply = h(MessageReply, { author: props.author?.display_name || props.author!.username, text: props.text || "", id: props.message.uuid, replyId: props.replyMessage?.uuid || element.dataset.messageId!, maxWidth: "full" });
+		messageBox.prepend(div);
+		render(messageReply, div);
+	}
 }
