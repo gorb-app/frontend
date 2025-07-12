f5457f9965db776f5e4ec8d6410c012fadf6b1bc
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-07-12 19:11:47 +0200

    fix: remove "N/A online" from all friends page

--- components/Me/FriendsList.vue
+++ components/Me/FriendsList.vue
@@ -1,11 +1,11 @@
 <template>
 	<input id="search-friend-bar" placeholder="search"/>
 
-	<!-- we aren't checking for the #all hash, since this is the default and fallback one -->
+	<!-- we aren't checking for the "all" variant, since this is the default and fallback one -->
 
 	<p v-if="props.variant === 'online'" style="text-align: left;">Online – 0</p>
 	<p v-else-if="props.variant === 'pending'" style="text-align: left;">Friend Requests – 0</p>
-	<p v-else style="text-align: left;">Friends – {{ friends?.length || 0 }} (N/A Online)</p>
+	<p v-else style="text-align: left;">Friends – {{ friends?.length || 0 }}</p>
 
 	<div id="friends-list">
 		<div v-if="props.variant === 'online'">
