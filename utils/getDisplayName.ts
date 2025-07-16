d5b766929140ff90627f00752a6c6121ef838def
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-07-16 11:25:36 +0200

    fix: allow optional argument instead of defaulting to undefined

--- utils/getDisplayName.ts
+++ utils/getDisplayName.ts
@@ -1,6 +1,6 @@
 import type { GuildMemberResponse, UserResponse } from "~/types/interfaces";
 
-export function getDisplayName(user: UserResponse, member: GuildMemberResponse | undefined = undefined): string {
+export function getDisplayName(user: UserResponse, member?: GuildMemberResponse): string {
 	if (member?.nickname) return member.nickname
 	if (user.display_name) return user.display_name
 	return user.username
