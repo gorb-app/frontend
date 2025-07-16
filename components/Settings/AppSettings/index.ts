788222967b88284e6fede8bddb9356c10c445ec4
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-07-16 05:03:18 +0200

    style: clean up imports for settings page

---
+++ components/Settings/AppSettings/index.ts
@@ -0,0 +1,11 @@
+import Appearance from './Appearance.vue';
+import Notifications from './Notifications.vue';
+import Keybinds from './Keybinds.vue';
+import Language from './Language.vue';
+
+export {
+	Appearance,
+	Notifications,
+	Keybinds,
+	Language,
+}
