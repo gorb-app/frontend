0ea12e7f0056cde27e44b4d2319304162d1f603c
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-07-18 11:36:06 +0200

    feat: HorizontalSpacer

---
+++ components/UserInterface/HorizontalSpacer.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="horizontal-spacer"></span>
+</template>
+
+<style scoped>
+.spacer {
+  display: block;
+  width: 0.2dvh;
+  margin: 0.2dvh 0.8dvw;
+  background-color: var(--padding-color);
+}
+</style>
