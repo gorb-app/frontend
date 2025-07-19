834c9ac343dbb234eedc30b9ee9f11230ba1545a
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-07-19 16:56:14 +0200

    fix: update sortMembers to use the new getDisplayName

--- utils/sortMembers.ts
+++ utils/sortMembers.ts
@@ -2,6 +2,6 @@ import type { GuildMemberResponse } from "~/types/interfaces";
 
 export default (members: GuildMemberResponse[]): GuildMemberResponse[] => {
 	return members.sort((a, b) => {
-		return getDisplayName(a.user, a).localeCompare(getDisplayName(b.user, b))
+		return getDisplayName(a).localeCompare(getDisplayName(b))
 	})
 }
