bf303f478e5c36942de1120701333f47b63b046d
Temmie <git@beaver.mom>
2025-08-05 05:26:30 +0200

    fix: remove accidental console log

--- components/Guild/ChannelEntry.vue
+++ components/Guild/ChannelEntry.vue
@@ -13,7 +13,6 @@
 
 <script lang="ts" setup>
 
-console.log("CHANNEL!")
 const props = defineProps<{ name: string, uuid: string, currentUuid: string, href: string }>();
 
 const isCurrentChannel = props.uuid == props.currentUuid;
