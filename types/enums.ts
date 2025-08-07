4dbe548efc07110be3329f9658e0edc5010d3e66
SauceyRed <saucey@saucey.red>
2025-08-07 06:31:15 +0200

    feat: create enums.ts file and add Permission enum

---
+++ types/enums.ts
@@ -0,0 +1,11 @@
+export const enum Permission {
+	SendMessage = 1,
+	ManageChannel = 2,
+	ManageRole = 4,
+	CreateInvite = 8,
+	ManageInvite = 16,
+	ManageGuild = 32,
+	ManageMember = 64,
+	BanMember = 128,
+	KickMember = 256
+}
