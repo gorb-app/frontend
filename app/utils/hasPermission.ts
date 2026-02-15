a5ded539444a80269ab96d059a0bb5342ea8aa8d
SauceyRed <saucey@saucey.red>
2026-02-15 10:18:32 +0100

    chore: add logging in hasPermission utility function

--- app/utils/hasPermission.ts
+++ app/utils/hasPermission.ts
@@ -2,10 +2,14 @@ import type { Permission } from "~/types/enums";
 import type { GuildMemberResponse } from "~/types/interfaces";
 
 export default (member: GuildMemberResponse, permission: Permission) => {
+	console.log(`[hasPermission] Checking if any of ${member.user.display_name}'s roles has ${permission.toString()} permission`);
 	for (const role of member.roles) {
+		console.log(`[hasPermission] Checking ${role.name}`);
 		if (role.permissions & permission) {
+			console.log(`[hasPermission] ${role.name} has ${permission.toString()} permission`);
 			return true;
 		}
+		console.log(`[hasPermission] ${role.name} does not have ${permission.toString()} permission`);
 	}
 	return false;
 }
